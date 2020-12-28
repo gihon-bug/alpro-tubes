8fa8bf9812a0dabefc87c0b5b308143351e58fb3
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-28 18:05:53 +0800

    add Add PushButton to add modul

--- gui/perhitungan_widget.py
+++ gui/perhitungan_widget.py
@@ -11,6 +11,11 @@ class PerhitunganWidget( QWidget ):
         self.layout = QVBoxLayout( self )
         self.layout.setAlignment( Qt.AlignTop )
 
+        self._add_button = QPushButton( "Add", self )
+        self._add_button.clicked.connect( self.add_modul )
+
+        self.layout.addWidget( self._add_button )
+
     def set_modul( self, modul ):
         if self._modul != modul:
             self._modul = modul
