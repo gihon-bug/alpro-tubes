ee4e229ce9eda3907c658a935ea5c1fa5bd481e9
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-28 17:53:55 +0800

    add modul button

--- gui/button_widget.py
+++ gui/button_widget.py
@@ -1,26 +1,29 @@
-from PyQt5.QtWidgets import QGroupBox, QPushButton, QVBoxLayout
-from PyQt5.QtCore import pyqtSignal
-from base.modul import Modul
+from PyQt5.QtWidgets import QGroupBox, QVBoxLayout
+from PyQt5.QtCore import pyqtSignal, Qt
+from .modul_button import ModulButton
 
 class ButtonWidget( QGroupBox ):
-    clicked = pyqtSignal()
+    clicked = pyqtSignal( object )
     def __init__( self, content : set ):
         super().__init__( content )
 
         self.layout = QVBoxLayout()
+        self.layout.setAlignment( Qt.AlignTop )
         self.setLayout( self.layout )
 
         self._modul = {}
+        self._button = []
 
     def set_modul( self, list_modul ):
         self._modul = list_modul
+        self._button.clear()
 
-        self.button = []
         for name, value in list_modul.items():
-            print(name)
-            button = QPushButton( name, self )
+            button = ModulButton( name, self )
+            button.set_modul( value )
+            button.clicked.connect(  self.onButtonClicked )
             self.layout.addWidget( button )
-            self.button.append( button )
+            self._button.append( button )
 
-    def onButtonClicked( self ):
-        pass
+    def onButtonClicked( self, modul ):
+        self.clicked.emit( modul )
--- gui/modul_button.py
+++ gui/modul_button.py
@@ -1,7 +1,22 @@
 from PyQt5.QtWidgets import QPushButton
+from PyQt5 import QtCore
+from base.modul import Modul
 
 class ModulButton( QPushButton ):
-    def __init__( self, modul ):
-        if modul is Modul:
-            
-        super().__init__(  )
+    clicked = QtCore.pyqtSignal( object )
+
+    def __init__( self, name, parent=None ):
+        super().__init__( name )
+        self._name : str
+        self._modul : Modul
+
+    def set_modul( self,  modul ):
+        if issubclass( modul, Modul ):
+            self._modul = modul
+
+    def get_modul( self ):
+        return self._modul
+
+    def hitButton( self, event ):
+        self.clicked.emit( self._modul )
+        return False
