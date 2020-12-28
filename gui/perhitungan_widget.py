0661d19e5354d6e609b99c1cc8ad3b3ea74cd8b4
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-28 18:03:28 +0800

    add layout aligned top

--- gui/perhitungan_widget.py
+++ gui/perhitungan_widget.py
@@ -1,10 +1,15 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QPushButton
+from PyQt5.QtCore import Qt
 from base.modul import Modul
 
 class PerhitunganWidget( QWidget ):
     def __init__( self, parent=None ):
         super().__init__( parent )
         self._modul : Modul
+        self._modul = None
+
+        self.layout = QGridLayout( self )
+        self.layout.setAlignment( Qt.AlignTop )
 
     def set_modul( self, modul ):
         self._modul = modul
