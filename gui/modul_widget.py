7eda3d5c0fc66cf8c82921620d28fa892458e136
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-28 21:31:37 +0800

    set ModulWidget layout

--- gui/modul_widget.py
+++ gui/modul_widget.py
@@ -1,5 +1,8 @@
-from PyQt5.QtWidgets import QGroupBox
+from PyQt5.QtWidgets import QGroupBox, QVBoxLayout
+from PyQt5.QtCore import Qt
 
 class ModulWidget( QGroupBox ):
     def __init__( self, parent=None ):
         super().__init__( parent )
+        self.layout = QVBoxLayout( self )
+        self.layout.setAlignment( Qt.AlignTop )
