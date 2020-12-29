29a3f299d8a92a80455255a76511ae1f54bae176
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-29 13:35:50 +0800

    add option and callable to calculate value

--- gui/line_edit.py
+++ gui/line_edit.py
@@ -4,6 +4,17 @@ class LineEdit( QLineEdit ):
     def __init__( self, name, parent=None ):
         super().__init__( "" , parent )
         self._name = name
-    
+        self._option = {}
+        self._calc = lambda value : 0
+
     def get_name( self ):
         return self._name
+
+    def set_option( self, option : dict ):
+        self._option = option
+
+    def set_calculation( self, func ):
+        self._calc = func
+
+    def calc_value( self, value ):
+        self.setText( str( self._calc( value ) ) )
