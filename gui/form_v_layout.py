6a16eeeace98456b82089acc872bf5b7ce774d4f
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-29 08:55:00 +0800

    make QFormLayout Vertically

---
+++ gui/form_v_layout.py
@@ -0,0 +1,23 @@
+from PyQt5.QtWidgets import QGridLayout, QWidget, QLabel, QLayout
+
+class FormVLayout( QGridLayout ):
+    def __init__( self, parent=None ):
+        super().__init__( parent )
+
+    def addWidget( self, name, widget : QWidget, row : int, col : int ):
+        label = QLabel( name )
+
+        super().addWidget( label, row * 2, col )
+        super().addWidget( widget, row * 2 + 1, col )
+
+    def addItem( self, name, item, row, col ):
+        label = QLabel( name )
+
+        super().addWidget( label, row * 2, col )
+        super().addItem( item, row * 2 + 1, col )
+
+    def addLayout( self, name, layout : QLayout, row, col ):
+        label = QLabel( name )
+
+        super().addWidget( label, row, * 2, col )
+        super().addLayout( layout, row * 2 + 1, col )
