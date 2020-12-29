4c87b4b327c9195fc835c37b5184ff990188d981
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-29 09:16:11 +0800

    change LineEditWidget to LineEditGroup

--- gui/line_edit_group.py
+++ gui/line_edit_group.py
@@ -3,7 +3,7 @@ from PyQt5.QtCore import Qt
 from .form_v_layout import FormVLayout
 from .line_edit import LineEdit
 
-class LineEditWidget( QGroupBox ):
+class LineEditGroup( QGroupBox ):
     def __init__( self, parent=None ):
         super().__init__( parent )
         self.layout = FormVLayout( self )
