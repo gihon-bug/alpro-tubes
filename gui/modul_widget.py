0c32102dee7b260b4b982868aac92aaac8a3bddf
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-29 11:27:38 +0800

    add ModulGroup

--- gui/modul_widget.py
+++ gui/modul_widget.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QGroupBox, QVBoxLayout, QHBoxLayout, QPushButton, QSizePolicy, QScrollArea, QWidget
 from PyQt5.QtCore import Qt
 from .perhitungan_widget import PerhitunganWidget
+from .modul_group import ModulGroup
 
 class ModulWidget(  QGroupBox ):
     def __init__( self, parent=None ):
@@ -22,38 +23,27 @@ class ModulWidget(  QGroupBox ):
 
         self.layout.setAlignment( Qt.AlignTop )
 
-        self.scroll_area = QScrollArea( self )
-        self.scroll_area.setSizePolicy( QSizePolicy.Minimum, QSizePolicy.MinimumExpanding )
-        self.scroll_layout = QVBoxLayout( self.scroll_area )
-        self.scroll_layout.setAlignment( Qt.AlignTop )
-
-        self.modul_group = QWidget( self )
-        self.scroll_area.setWidget( self.modul_group )
-        self.scroll_area.setWidgetResizable( True )
-
-        self.layout.addWidget( self.scroll_area )
-
         self._add_button.clicked.connect( self.add_modul )
         self._remove_button.clicked.connect( self.pop_modul )
         self._clear_modul_button.clicked.connect( self.clear_modul )
         self._clear_value_button.clicked.connect( self.clear_value )
 
+        self._modul_group = ModulGroup( self )
+
+        self.layout.addWidget( self._modul_group )
+
         self._modul = None
 
     def set_modul( self, modul ):
         self._modul = modul
+        self._modul_group.set_modul( modul )
         self.add_modul()
 
     def get_modul( self ):
         return self._modul
 
     def add_modul( self ):
-        if self._modul is not None:
-            widget = PerhitunganWidget( self )
-            widget.set_modul( self._modul )
-            widget.setSizePolicy( QSizePolicy.Minimum, QSizePolicy.Minimum )
-            self.modul_layout.addWidget( widget )
-            widget.adjustSize()
+        self._modul_group.add_modul()
 
     def pop_modul( self ):
         pass
