a69174c4db76561cf8b969fd65c9b28cbeb52f5d
Uskrai <30656271+Uskrai@users.noreply.github.com>
2020-12-29 11:27:21 +0800

    fix no self._interfaces error

--- gui/perhitungan_widget.py
+++ gui/perhitungan_widget.py
@@ -10,7 +10,6 @@ class PerhitunganWidget( QGroupBox ):
         super().__init__( parent )
         self._modul : Modul
         self._modul = None
-        self.set_modul( modul )
 
         self.layout = QHBoxLayout( self )
         self.setLayout( self.layout )
@@ -28,13 +27,14 @@ class PerhitunganWidget( QGroupBox ):
         self.layout.addWidget( self._user_input, 3 )
         self.layout.addWidget( self._user_output, 1 )
 
+        self.set_modul( modul )
+
     def set_modul( self, modul ):
         if self._modul is not modul:
             self._modul = modul
             self._interfaces.set_name( modul.name )
 
-            modul_inst = modul()
-            modul_inst.get_value( self._interfaces )
+            modul.get_value( self._interfaces )
 
 
     def clear_modul( self ):
